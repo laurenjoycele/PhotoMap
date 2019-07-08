2485fa4a23ad6fb8660fc87b18ee000110b2a9df
Lauren Le <laurenjle@fb.com>
2019-07-08 12:38:05 -0700

    take a photo works milestone 3 works

--- PhotoMap/View Controllers/PhotoMapViewController.m
+++ PhotoMap/View Controllers/PhotoMapViewController.m
@@ -9,7 +9,7 @@
 #import "PhotoMapViewController.h"
 #import <MapKit/MapKit.h>
 
-@interface PhotoMapViewController ()
+@interface PhotoMapViewController () <UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (weak, nonatomic) IBOutlet UIButton *cameraButton;
 
@@ -23,6 +23,43 @@
     //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
     MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
     [self.mapView setRegion:sfRegion animated:false];
+    
+    
+    
+
+}
+
+//implement delegate method
+//When the user finishes taking the picture, UIImagePickerController returns a dictionary that contains the image and some other meta data
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    
+    // Do something with the images (based on your use case)
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion: ^(){[self performSegueWithIdentifier:@"firstSegue" sender:nil];}];
+}
+
+- (IBAction)pressedCameraButton:(id)sender {
+    //instantiate UIImagePickerController
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    //imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+    
+    //check if camera is supported on device
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
