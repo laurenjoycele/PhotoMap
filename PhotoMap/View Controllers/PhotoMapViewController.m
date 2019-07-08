ec3257382689731d65ff26079f9828f91991064e
Lauren Le <laurenjle@fb.com>
2019-07-08 10:54:56 -0700

    :initial commit

---
+++ PhotoMap/View Controllers/PhotoMapViewController.m
@@ -0,0 +1,43 @@
+//
+//  PhotoMapViewController.m
+//  PhotoMap
+//
+//  Created by emersonmalca on 7/8/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+#import "PhotoMapViewController.h"
+#import <MapKit/MapKit.h>
+
+@interface PhotoMapViewController ()
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UIButton *cameraButton;
+
+@end
+
+@implementation PhotoMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
+    MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
+    [self.mapView setRegion:sfRegion animated:false];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
